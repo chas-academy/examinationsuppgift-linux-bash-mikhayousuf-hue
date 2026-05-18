97cf2715977cb40f002c9c85b7cb219ac4b0ec94
mikhayousuf-hue <mikhayousuf@gmail.com>
2026-05-18 11:56:46 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,46 +1,36 @@
 #!/bin/bash
 
-# Kontrollera att scriptet körs som root
+# Kontrollera root
 if [ "$EUID" -ne 0 ]; then
     echo "Du måste vara root för att köra scriptet."
     exit 1
 fi
 
-# Kontrollera att användare skickas in
+# Kontrollera argument
 if [ $# -eq 0 ]; then
     echo "Användning: $0 användare"
     exit 1
 fi
 
-# Loopa igenom alla användare
+# Loopa genom användare
 for user in "$@"
 do
-    # Skapa användare och hemkatalog
     useradd -m "$user"
 
-    # Hemkatalog
     HOME_DIR="/home/$user"
 
-    # Skapa undermappar
     mkdir -p "$HOME_DIR/Documents"
     mkdir -p "$HOME_DIR/Downloads"
     mkdir -p "$HOME_DIR/Work"
 
-    # Endast ägaren får åtkomst
     chmod 700 "$HOME_DIR/Documents"
     chmod 700 "$HOME_DIR/Downloads"
     chmod 700 "$HOME_DIR/Work"
 
-    # Skapa välkomstfil
     echo "Välkommen $user" > "$HOME_DIR/welcome.txt"
+    echo "Alla användare:" >> "$HOME_DIR/welcome.txt"
+    cat /etc/passwd >> "$HOME_DIR/welcome.txt"
 
-    # Lista alla användare i systemet
-    cut -d: -f1 /etc/passwd >> "$HOME_DIR/welcome.txt"
-
-    # Rättigheter för välkomstfil
     chmod 600 "$HOME_DIR/welcome.txt"
-
-    # Gör användaren till ägare
     chown -R "$user:$user" "$HOME_DIR"
-
 done
