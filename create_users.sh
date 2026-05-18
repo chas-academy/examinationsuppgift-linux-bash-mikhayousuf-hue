a50b16eaf630ed3c54a4c14727da0b9b7176528b
mikhayousuf-hue <mikhayousuf@gmail.com>
2026-05-18 12:03:20 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -15,22 +15,28 @@ fi
 # Loopa genom användare
 for user in "$@"
 do
+    # Skapa användaren med hemkatalog
     useradd -m "$user"
 
     HOME_DIR="/home/$user"
 
+    # Skapa undermappar
     mkdir -p "$HOME_DIR/Documents"
     mkdir -p "$HOME_DIR/Downloads"
     mkdir -p "$HOME_DIR/Work"
 
+    # Sätt rättigheter (endast ägaren har tillgång)
     chmod 700 "$HOME_DIR/Documents"
     chmod 700 "$HOME_DIR/Downloads"
     chmod 700 "$HOME_DIR/Work"
 
+    # Skapa personligt välkomstmeddelande
     echo "Välkommen $user" > "$HOME_DIR/welcome.txt"
-    echo "Alla användare:" >> "$HOME_DIR/welcome.txt"
-    cat /etc/passwd >> "$HOME_DIR/welcome.txt"
+    
+    # Skriv ut en ren lista på alla andra användare i systemet
+    cut -d: -f1 /etc/passwd | grep -v "^$user$" >> "$HOME_DIR/welcome.txt"
 
+    # Sätt rättigheter och ägare för välkomstfilen och hemkatalogen
     chmod 600 "$HOME_DIR/welcome.txt"
     chown -R "$user:$user" "$HOME_DIR"
 done
