b6634439446df2f9c095d2ca77edef89836420e6
mikhayousuf-hue <mikhayousuf@gmail.com>
2026-05-19 11:34:07 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-# Kontrollera root
+# Kontrollera att scriptet körs som root
 if [ "$EUID" -ne 0 ]; then
     echo "Du måste vara root för att köra scriptet."
     exit 1
 fi
 
-# Kontrollera argument
+# Kontrollera att minst ett användarnamn skickas in
 if [ $# -eq 0 ]; then
     echo "Användning: $0 användare"
     exit 1
 fi
 
-# Loopa genom användare
+# Loopa igenom alla användare
 for user in "$@"
 do
-    # Skapa användaren med hemkatalog
+    # Skapa användare med hemkatalog
     useradd -m "$user"
 
-    HOME_DIR="./"
+    # Sätt hemkatalog
+    HOME_DIR="/home/$user"
 
-    # Skapa undermappar
+    # Skapa mappar i användarens hemkatalog
     mkdir -p "$HOME_DIR/Documents"
     mkdir -p "$HOME_DIR/Downloads"
     mkdir -p "$HOME_DIR/Work"
 
-    # Sätt rättigheter (endast ägaren har tillgång)
+    # Endast ägaren får läsa skriva och gå in
     chmod 700 "$HOME_DIR/Documents"
     chmod 700 "$HOME_DIR/Downloads"
     chmod 700 "$HOME_DIR/Work"
 
-    # Skapa personligt välkomstmeddelande
-    echo "Välkommen $user" > "./welcome.txt"
-    
-    # Skriv ut en ren lista på alla andra användare i systemet
-    cut -d: -f1 /etc/passwd | grep -v "^$user$" >> "./welcome.txt"
+    # Skapa welcome.txt i användarens hemkatalog
+    echo "Välkommen $user" > "$HOME_DIR/welcome.txt"
 
-    # Sätt rättigheter och ägare för välkomstfilen och hemkatalogen
-    chmod 600 "./welcome.txt"
-    chown -R "$user:$user" "./"
+    # Lägg till alla andra användare
+    cut -d: -f1 /etc/passwd | grep -v "^$user$" >> "$HOME_DIR/welcome.txt"
+
+    # Sätt rätt ägare och rättigheter
+    chown -R "$user:$user" "$HOME_DIR"
+    chmod 600 "$HOME_DIR/welcome.txt"
 done
