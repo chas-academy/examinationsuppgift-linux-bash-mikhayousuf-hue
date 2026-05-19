7cd924f00238dad015f18ae07a1c15b0aa2c38d6
mikhayousuf-hue <mikhayousuf@gmail.com>
2026-05-19 23:31:30 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -12,29 +12,35 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Lista gamla användare
-OLD_USERS=$(cut -d: -f1 /etc/passwd)
-
 # Loopa genom användare
 for user in "$@"
 do
+    # Hämta gamla användare innan skapning
+    OLD_USERS=$(cut -d: -f1 /etc/passwd)
+
+    # Skapa användare
     useradd -m "$user"
 
     HOME_DIR="/home/$user"
 
+    # Skapa mappar
     mkdir -p "$HOME_DIR/Documents"
     mkdir -p "$HOME_DIR/Downloads"
     mkdir -p "$HOME_DIR/Work"
 
+    # Rättigheter
     chmod 700 "$HOME_DIR/Documents"
     chmod 700 "$HOME_DIR/Downloads"
     chmod 700 "$HOME_DIR/Work"
 
+    # Welcome-fil
     echo "Välkommen $user" > "$HOME_DIR/welcome.txt"
 
-    echo "$OLD_USERS" >> "$HOME_DIR/welcome.txt"
+    # Lägg till andra användare
+    echo "$OLD_USERS" | grep -v "^$user$" >> "$HOME_DIR/welcome.txt"
 
+    # Ägare och rättigheter
+    chown -R "$user:$user" "$HOME_DIR"
     chmod 600 "$HOME_DIR/welcome.txt"
 
-    chown -R "$user:$user" "$HOME_DIR"
 done
